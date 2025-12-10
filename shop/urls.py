e71840e6fab952f13eb14fa7ae9dab7d34d25d0b
Arathy <arathyrajeesh2@gmail.com>
2025-12-10 13:03:36 +0530

    forget password added

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.hero, name="home"),
@@ -78,4 +79,19 @@ urlpatterns = [
     path("shop/<int:shop_id>/rate/", views.rate_shop, name="rate_shop"),
     path("service/<int:service_id>/rate/", views.rate_service, name="rate_service"),
     path("branch/<int:branch_id>/rate/", views.rate_branch, name="rate_branch"),
+    path("password_reset/",
+            auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+            name="password_reset"),
+
+    path("password_reset_done/",
+            auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
+            name="password_reset_done"),
+
+    path("reset/<uidb64>/<token>/",
+            auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+            name="password_reset_confirm"),
+
+    path("reset/done/",
+            auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
+            name="password_reset_complete"),
 ]
