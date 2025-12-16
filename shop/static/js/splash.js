ddbb248363e6b714dc6ae8a4b3eec35113557856
Arathy <arathyrajeesh2@gmail.com>
2025-12-16 19:58:53 +0530

    spalsh page added

--- shop/static/js/splash.js
+++ shop/static/js/splash.js
@@ -1,3 +1,10 @@
-setTimeout(() => {
-    window.location.href = "/home/";
-}, 4000); // 4 seconds
+// Android-style splash timing
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".splash").classList.add("exit");
+    }, 3000); // show splash for 3 seconds
+
+    setTimeout(() => {
+        window.location.href = "/home/";
+    }, 3800); // redirect after fade-out
+});
