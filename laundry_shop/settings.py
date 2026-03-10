a852ef4aea73d3d6ffad098af8058596e8d5ffe9
arathyrajeesh <arathyrajeesh2@gmail.com>
2026-03-10 16:56:58 +0530

    fix postgres ssl

--- laundry_shop/settings.py
+++ laundry_shop/settings.py
@@ -153,7 +153,7 @@ WSGI_APPLICATION = 'laundry_shop.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
